bace74257f1c1b866216f1d7d5cc06b59d7851a6
BillH <wmhhardgroveiv@gmail.com>
2022-07-10 04:46:09 -0400

    resume mobile view fixed

--- bh-portfolio/src/pages/Resume.js
+++ bh-portfolio/src/pages/Resume.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import "../styles/Resume.css"
 
 function Resume() {
   return (
     <div>
-      <iframe src="https://resume.creddle.io/embed/fz3ypu8xoqt"
+      <iframe className='resume'  src="https://resume.creddle.io/embed/fz3ypu8xoqt"
   width="850" height="1100" seamless></iframe>
     </div>
   )
