42026b9783821ab2f0c72310db0db0efcace249d
BillH <wmhhardgroveiv@gmail.com>
2022-07-10 04:00:42 -0400

    resume loads

--- bh-portfolio/src/pages/Resume.js
+++ bh-portfolio/src/pages/Resume.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Resume() {
   return (
-    <div>Resume</div>
+    <div>
+      <iframe src="https://resume.creddle.io/embed/fz3ypu8xoqt"
+  width="850" height="1100" seamless></iframe>
+    </div>
   )
 }
 
